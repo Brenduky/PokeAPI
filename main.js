9650174b24bb967ece1951da5d587debb82f9213
Brenda Martinez <bvergaram1500@alumno.ipn.mx>
2021-05-09 20:12:53 -0500

    funcional, tarjetas repetidas

--- main.js
+++ main.js
@@ -1,30 +1,45 @@
 let contenedor = document.getElementById("contenedorPokemon")
 
-    const consultarPokemon = async (id, num) => {
+ const consultarPokemon = async (id, num) => {
         const resultado = await fetch (`https://pokeapi.co/api/v2/pokemon/${id}`);
-        const datos = await resultado.json()
-        const pokemones = datos.results
+        const pokemon = await resultado.json()
 
-        const result = pokemones 
-        .map((pokemon) => crearPokemon(pokemon, num))
-        .join(' ')
+        console.log(pokemon)
+       
+        const result = crearPokemon(pokemon,num)
 
         contenedor.innerHTML = result
-        console.log(pokemones)
 
-    }
+}
 
-        function consultarPokemones() {
+function consultarPokemones() {
             let primerID = Math.round(Math.random()*150)
             let segundoID = Math.round(Math.random()*150) 
 
             consultarPokemon(primerID, 1)
             consultarPokemon(segundoID, 2)
-        }
+}
 
-        function crearPokemon(pokemon,num) {
-            return `holiwi`
+
+const crearPokemon = ({sprites,name,base_experience},num) => {
+    return `        
+    <div class="Pokemon">
+        <img src="${sprites.front_default}" alt="${name} Sprite">
+        <p>Pokemon${num}</p>
+        <p>${name}</p>
+        <p>${base_experience}</p>
+    </div>        
+
+    <div class="vs">VS</div>
+
+    <div class="Pokemon">
+        <img src="${sprites.front_default}" alt="${name} Sprite">
+        <p>Pokemon${num}</p>
+        <p>${name}</p>
+        <p>${base_experience}</p>
+    </div>
+    `
             
-        }
+}
 
-        consultarPokemones()
+    consultarPokemones()
