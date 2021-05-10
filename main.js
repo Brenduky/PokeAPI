5abdc8d649b1c33ab471965a78dd9c6bdad25a55
Brenda Martinez <bvergaram1500@alumno.ipn.mx>
2021-05-09 19:10:30 -0500

    version1

---
+++ main.js
@@ -0,0 +1,25 @@
+    let contenedor = document.getElementById("contenedorPokemon")
+
+       function consultarPokemon(id, num) {
+            fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+            .then(function (response) {
+            response.json()
+            .then(function (pokemon) {
+                crearPokemon(pokemon,num)
+            })
+        })
+        }
+
+        function consultarPokemones() {
+            let primerID = Math.round(Math.random()*150)
+            let segundoID = Math.round(Math.random()*150) 
+
+            consultarPokemon(primerID, 1)
+            consultarPokemon(segundoID, 2)
+        }
+
+        function crearPokemon(pokemon,num) {
+            
+        }
+
+        consultarPokemones()
